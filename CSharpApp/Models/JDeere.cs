adf214a702da33dba7bf5b772d97780b3d95d29d
Sean Mooney <bable4@gmail.com>
2020-10-31 10:09:47 -0500

    Follow the 'connections' uri when present
    * Full access to the API requires both user consent and designating
    which organizations the user wants the application to be able to
    interact with. Follow the 'connections' uri if present on any org to
    allow the user finish the setup process.
    * The connections app knows how to issue a redirct when the
    select-organization route is opened with a `redirect_uri` query parameter.
    This can send the user back to the original app once they have completed
    the organization selection process and makes exercising the api through
    the demo app smoother.

--- CSharpApp/Models/JDeere.cs
+++ CSharpApp/Models/JDeere.cs
@@ -1,10 +1,11 @@
-﻿namespace CSharpApp.Models 
+﻿namespace CSharpApp.Models
 {
     public class JDeere
     {
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string WellKnown { get; set; }
+        public string ServerUrl { get; set; }
         public string CallbackUrl { get; set; }
         public Token AccessToken { get; set; }
         public string Scopes { get; set; }
